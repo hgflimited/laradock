eb5b401f6014fbf57191566598106f4ab24e850e
Patrick Henderson <pathend98@gmail.com>
2019-07-01 09:13:44 +0100

    Configuring dockerfiles for qa

--- ide-codiad/config.php
+++ ide-codiad/config.php
@@ -18,7 +18,7 @@ define("WHITEPATHS", BASE_PATH . ",/home,/");
 // SESSIONS (e.g. 7200)
 $cookie_lifetime = "0";
 // TIMEZONE
-date_default_timezone_set(getenv('TZ','UTC'));
+date_default_timezone_set(getenv('TZ', 'UTC'));
 // Allows to overwrite the default language
 //define("LANGUAGE", "en");
 // External Authentification
@@ -31,13 +31,12 @@ define("COMPONENTS", BASE_PATH . "/components");
 define("PLUGINS", BASE_PATH . "/plugins");
 define("THEMES", BASE_PATH . "/themes");
 define("DATA", BASE_PATH . "/data");
-define("WORKSPACE", getenv('APP_CODE_PATH_CONTAINER',BASE_PATH . "workspace"));
+define("WORKSPACE", getenv('APP_CODE_PATH_CONTAINER', BASE_PATH . "workspace"));
 // URLS
-define("WSURL", getenv('APP_CODE_PATH_CONTAINER',BASE_PATH . "workspace"));
+define("WSURL", getenv('APP_CODE_PATH_CONTAINER', BASE_PATH . "workspace"));
 // Marketplace
 define("MARKETURL", "http://market.codiad.com/json");
 // Update Check
 //define("UPDATEURL", "http://update.codiad.com/?v={VER}&o={OS}&p={PHP}&w={WEB}&a={ACT}");
 define("ARCHIVEURL", "https://github.com/Codiad/Codiad/archive/master.zip");
 define("COMMITURL", "https://api.github.com/repos/Codiad/Codiad/commits");
-?>
